b59633f8ae4089c491b3325c55dddcf388151c4c
Wen1750686723 <wen1750686723@qq.com>
2015-05-25 16:35:03 +0800

    second commit

---
+++ weixin.php
@@ -0,0 +1,102 @@
+<?php
+/**
+  * wechat php test
+  */
+
+//define your token
+define("TOKEN", "liuwenbohhh");
+$wechatObj = new wechatCallbackapiTest();
+$wechatObj->responseMsg();
+
+class wechatCallbackapiTest
+{
+	public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        //valid signature , option
+        if($this->checkSignature()){
+        	echo $echoStr;
+        	exit;
+        }
+    }
+
+    public function responseMsg()
+    {
+		//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+      	//extract post data
+		if (!empty($postStr)){
+                
+              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";             
+				if(!empty( $keyword ))
+                {
+                	$con = mysql_connect("lnmg-001.xincache.cn","host5305985","wenbo744037");
+					if (!$con)
+					  {
+					  die('Could not connect: ' . mysql_error());
+					  }
+
+					mysql_select_db("host5305985", $con);
+					mysql_query("set names utf8");
+
+					$result = mysql_query("SELECT * FROM text WHERE word='{$keyword}'");
+
+					while($row = mysql_fetch_array($result))
+					  {
+					  $contentStr=$row['text'];
+
+					  }
+
+					mysql_close($con);
+					if ($contentStr==null) {
+						$contentStr = "我是!";
+					}
+              		$msgType = "text";
+                	
+                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                	echo $resultStr;
+                }else{
+                	echo "Input something...";
+                }
+
+        }else {
+        	echo "";
+        	exit;
+        }
+    }
+		
+	private function checkSignature()
+	{
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];	
+        		
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+?>
