74e78d83e19a34a38d20f31867496a26f16ef418
Wen1750686723 <wen1750686723@qq.com>
2015-06-03 13:50:35 +0800

    index

--- index.php
+++ index.php
@@ -11,6 +11,27 @@
 应答语: <input type="text" name="email"><br>
 <input type="submit">
 </form>
+<table border="1">
+<?php
+$con = mysql_connect("lnmg-001.xincache.cn","host5305985","wenbo744037");
+					if (!$con)
+					  {
+					  die('Could not connect: ' . mysql_error());
+					  }
 
+					mysql_select_db("host5305985", $con);
+               mysql_query("set names utf8");
+
+               echo "<tr><th>应答语</th>" . "<th>回复语</th></tr>";
+$result = mysql_query("SELECT * FROM text");
+
+while($row = mysql_fetch_array($result))
+  {
+  echo "<tr><th>".$row['word'] . "</th><th>" . $row['text']."</th></tr>";
+  }
+
+mysql_close($con);
+?>
+</table>
 </body>
 </html>
